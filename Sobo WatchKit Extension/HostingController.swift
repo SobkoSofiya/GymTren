7280cb9b0a5fa9090b77d22edee4e43900b3f2b2
SobkoSofiya <sobkosofiya23@gmail.com>
2021-02-26 15:16:54 +0300

    Initial Commit

---
+++ Sobo WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  Sobo WatchKit Extension
+//
+//  Created by Sofi on 26.02.2021.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ Sobo WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  Sobo WatchKit Extension
+//
+//  Created by Sofi on 26.02.2021.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
